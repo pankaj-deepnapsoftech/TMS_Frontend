5529fa50e50401737474c953fb80332182ef9cbe
pankaj-deepnapsoftech <itsybizz@gmail.com>
2025-07-05 16:48:33 +0530

    forgot password added

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import AuthContextProvider from './context/AuthContext2';
 import TicketCreateProvider from './context/TicketCreateContext';
 import { ProfileProvider } from './context/UserProfileUpdateContext';
 import NotificationProvider from './context/NotificationContext';
+import ForgetPassContextProvider from './context/ForgetPassContext';
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -17,13 +18,15 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <ToastContainer />
       <AuthContextProvider>
-        <ProfileProvider>
-          <TicketCreateProvider>
-            <NotificationProvider>
-              <App />
-            </NotificationProvider>
-          </TicketCreateProvider>
-        </ProfileProvider>
+        <ForgetPassContextProvider>
+          <ProfileProvider>
+            <TicketCreateProvider>
+              <NotificationProvider>
+                <App />
+              </NotificationProvider>
+            </TicketCreateProvider>
+          </ProfileProvider>
+        </ForgetPassContextProvider>
       </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>
